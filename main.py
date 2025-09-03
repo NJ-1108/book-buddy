45c637ebbd4a3ae7fa34fdc4f4009e519fc1425d
NJ-1108 <niveditha.jayamadhavan@gmail.com>
2025-09-03 19:56:53 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+import os 
 app = Flask(__name__)
 
 # Helper to convert list of (title, link) to list of dicts
@@ -588,4 +588,5 @@ def result():
                            suggestions=suggestions)
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
